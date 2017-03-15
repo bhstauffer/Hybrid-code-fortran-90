7df62c78ea935c9967b08df3776c1cc5430da88c
bhstauffer <bhstauffer@alaska.edu>
2017-03-15 12:14:53 -0800

    2d update for small scale

--- dimensions.f90
+++ dimensions.f90
@@ -2,9 +2,9 @@ module dimensions
       implicit none
       save
 
-      integer, parameter:: nx = 3, ny = 201, nz = 401 !61,201
+      integer, parameter:: nx = 3, ny = 201, nz = 201 !61,201
 
-      integer*4, parameter:: Ni_max = 500000  
+      integer*4, parameter:: Ni_max = 250000  
       
 end module dimensions
       
--- part_init.f90
+++ part_init.f90
@@ -344,7 +344,7 @@ module part_init
 !                  vp(l,1) = -0.0*(exp(-(xp(l,3)-qz(nz/2))**2/(10.*delz)**2)
 !               x        *exp(-(xp(l,1)-qx(nx/2))**2/(10.*dx)**2))+vx
                   vp(l,1) = vx+ &
-                        114.0*(1+0.5*cos(8*pi*qy(j)/(qy(ny-1))))*(1+0.5*cos(8*pi*qz(k)/qz(nz-1)))
+                        57.0*(1+0.5*cos(8*pi*qy(j)/(qy(ny-1))))*(1+0.5*cos(8*pi*qz(k)/qz(nz-1)))
                        ! 114.0*exp(-((xp(l,2)-qy(ny/2))**2+(xp(l,3)-qz(nz/2))**2)/(10*dz_grid(nz/2))**2) !Gaussian velocity perturbation (20)
                   vp(l,2) = vy 
                   vp(l,3) = vz
